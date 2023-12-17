d8c5d77c7ed78b7503c7e39f1fcad08e7232200d
Milica206001 <mmilica206@gmail.com>
2023-12-17 23:09:02 +0100

    features login and transfer
    Zavrsene komponene Login i Transfer.

--- React/reactapp/src/Login.jsx
+++ React/reactapp/src/Login.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Login.css";
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="wrapper d-flex align-items-center justify-content-center w-100">
+      <div className="login ">
+        <h2 className="mb-3">Prijavite se</h2>
+        <form action="" className="needs-validation">
+          <div className="form-group was-validated mb-2">
+            <label htmlFor="username" className="form-label">
+              Korisničko ime
+            </label>
+            <input type="text" className="form-control" required />
+            <div className="invalid-feedback">
+              Molimo Vas da unesete korisničko ime
+            </div>
+          </div>
+          <div className="form-group was-validated mb-2">
+            <label htmlFor="password" className="form-label">
+              Šifra
+            </label>
+            <input type="password" className="form-control" required />
+            <div className="invalid-feedback">
+              Molimo Vas da unesete lozinku
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="btn d-block btn-success mx-auto mt-2"
+          >
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- React/reactapp/src/Transfer.jsx
+++ React/reactapp/src/Transfer.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 function Transfer() {
-  return <div>Transfer</div>;
+  return (
+    <div className="wrapper d-flex align-items-center justify-content-center w-100 ">
+      <div className="transfer bg-warning">
+        <h2 className="mb-3 ">Transfer novca</h2>
+        <form action="" className="needs-validation">
+          <div className="form-group was-validated mb-2">
+            <label htmlFor="recipient" className="form-label">
+              Primalac
+            </label>
+            <input type="text" className="form-control" required />
+            <div className="invalid-feedback">
+              Molimo Vas da unesete primaoca
+            </div>
+          </div>
+          <div className="form-group was-validated mb-2">
+            <label htmlFor="brojRacuna" className="form-label">
+              Broj racuna
+            </label>
+            <input type="password" className="form-control" required />
+            <div className="invalid-feedback">
+              Molimo Vas da unesete broj racuna primaoca
+            </div>
+            <div className="form-group was-validated mb-2">
+              <label htmlFor="amount" className="form-label">
+                Iznos
+              </label>
+              <input type="text" className="form-control" required />
+              <div className="invalid-feedback">
+                Molimo Vas da unesete iznos
+              </div>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="btn d-block btn-warning mx-auto mt-2"
+          >
+            Izvrši transfer
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Transfer;
