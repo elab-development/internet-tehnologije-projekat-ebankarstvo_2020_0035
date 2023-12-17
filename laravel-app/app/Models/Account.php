2a4f38bff417a2a69e89b045962c655be07e74e8
Aleksandra Milicevic <am20200366@student.fon.bg.ac.rs>
2023-12-17 19:17:33 +0100

    Kreairan model Account
    Kreairan je model Account (model,factory,seeder) koji je povezan sa modelom User.

---
+++ laravel-app/app/Models/Account.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    protected $fillable =['title'];
+    protected $guarded = ['id','number','type','balance'];
+
+    public function users (){
+        return $this->belongsTo(User::class);
+    }
+}
+
+
