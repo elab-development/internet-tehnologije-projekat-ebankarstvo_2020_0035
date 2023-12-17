56d0b1d07714ef2cbe7a4b9f6807777b5dba2b82
Aleksandra Milicevic <am20200366@student.fon.bg.ac.rs>
2023-12-18 00:38:00 +0100

    Seeders
    Definisan je 'DatabaseSeeders'.

--- laravel-app/database/migrations/2023_12_17_223657_create_categories_table.php
+++ laravel-app/database/migrations/2023_12_17_223657_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id()->unique();
             $table->string('title');
+            $table->string('category_number')->nullable();
             $table->timestamps();
         });
     }
--- laravel-app/database/seeders/DatabaseSeeder.php
+++ laravel-app/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Transaction;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +15,24 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
     
+        \App\Models\User::truncate();
+        \App\Models\Account::truncate();
+        \App\Models\Transaction::truncate();
+        \App\Models\Category::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Transaction::factory(3)->create();
+
+         \App\Models\User::factory()->create([
+            'name' => 'Petar Petrovic',
+            'email' => 'petar@example.com',
+        ]);
+
+        $user=\App\Models\User::factory()->create([
+            'name'=>'Ana Jovanovic'
+        ]);
+
+        \App\Models\Account::factory(2)->create([
+            'user_id'=>$user->id
+        ]);
     }
 }
