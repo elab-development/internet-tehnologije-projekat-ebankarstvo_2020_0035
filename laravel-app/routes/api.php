9824972f4436f4c44eb6e78946227dc3a6744a1b
Aleksandra Milicevic <am20200366@student.fon.bg.ac.rs>
2023-12-18 13:51:01 +0100

    Routes,Controlers
    Definisane su osnovne rute i kontroleri (index,show).

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\CategoryController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +22,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users', [UserController::class, 'index']); //api/users
+Route::get('/users/{id}', [UserController::class, 'show']); //api/users/id
+
+Route::get('/transactions', [TransactionController::class, 'index']); //api/transactions
+Route::get('/transactions/{id}', [TransactionController::class, 'show']);  //api/transactions/id
+
+Route::get('/accounts', [AccountController::class, 'index']); //api/accounts
+Route::get('/accounts/{id}', [AccountController::class, 'show']);  //api/accounts/id
+
+Route::get('/categories', [CategoryController::class, 'index']); //api/categories
+Route::get('/categories/{id}', [CategoryController::class, 'show']);  //api/categories/id
