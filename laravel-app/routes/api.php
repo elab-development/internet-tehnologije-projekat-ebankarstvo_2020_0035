fcd3ca7b7e59372a2a7ccd384e173074195dcfea
Milica206001 <mmilica206@gmail.com>
2023-12-25 14:20:57 +0100

    rute i autentifikacija
    Dodate nove rute i poceta autentifikacija, izmenjeno da greske vracaju podatke u jsonu.

---
+++ laravel-app/routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\API\AuthController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+Route::get('/users', [UserController::class, 'index']); //api/users
+Route::get('/users/{id}', [UserController::class, 'show']); //api/users/id
+
+Route::resource('transactions', TransactionController::class); //api/transactions
+//Route::get('/transactions/{id}', [TransactionController::class, 'show']);  //api/transactions/id
+
+Route::get('/accounts', [AccountController::class, 'index']); //api/accounts
+Route::get('/accounts/{id}', [AccountController::class, 'show']);  //api/accounts/id
+
+Route::get('/categories', [CategoryController::class, 'index']); //api/categories
+Route::get('/categories/{id}', [CategoryController::class, 'show']);  //api/categories/id
